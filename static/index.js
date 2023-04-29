12a231d3c151707c467ddf6cba7dbc813a671d2d
GNE13 <galimiel@gmail.com>
2023-04-29 19:05:32 +0300

    navbar dropdown menu in prog

--- static/index.js
+++ static/index.js
@@ -33,3 +33,19 @@ function Message(props){
 }
 
 reactDOM.render(<Message interval={5000} />, document.getElementById("messages"));
+
+var dropdowns = document.getElementsByClassName("dropdown");
+
+for (var i = 0; i < dropdowns.length; i++) {
+  dropdowns[i].addEventListener("mouseover", function() {
+    this.classList.add("open");
+    var dropdownMenu = this.getElementsByClassName("dropdown-menu")[0];
+    dropdownMenu.style.display = "block";
+  });
+
+  dropdowns[i].addEventListener("mouseout", function() {
+    this.classList.remove("open");
+    var dropdownMenu = this.getElementsByClassName("dropdown-menu")[0];
+    dropdownMenu.style.display = "none";
+  });
+}
