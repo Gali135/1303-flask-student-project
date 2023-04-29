de290d96da89f75ffa362e58cca64c4b795378ec
GNE13 <galimiel@gmail.com>
2023-04-29 19:50:45 +0300

    dropdown menu prog

--- static/index.js
+++ static/index.js
@@ -1,8 +1,24 @@
 
 var today = new Date();
 var date = today.toLocaleDateString();
-document.getElementById("localDate").innerHTML = date
+// document.getElementById("localDate").innerHTML = date
 
+document.addEventListener("DOMContentLoaded", function() {
+  var dropdowns = document.getElementsByClassName("dropdown");
+
+  for (var i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].addEventListener("mouseover", function() {
+      this.classList.add("open");
+      var dropdownMenu = this.getElementsByClassName("dropdown-menu")[0];
+      dropdownMenu.style.display = "block";
+    });
+    dropdowns[i].addEventListener("mouseout", function() {
+      this.classList.remove("open");
+      var dropdownMenu = this.getElementsByClassName("dropdown-menu")[0];
+      dropdownMenu.style.display = "none";
+    });
+  }
+});
 
 function addCourse() {
   alert("Corse was added succefuly !");
@@ -32,20 +48,7 @@ function Message(props){
   );
 }
 
-reactDOM.render(<Message interval={5000} />, document.getElementById("messages"));
+// reactDOM.render(<Message interval={5000} />, document.getElementById("messages"));
 
-var dropdowns = document.getElementsByClassName("dropdown");
 
-for (var i = 0; i < dropdowns.length; i++) {
-  dropdowns[i].addEventListener("mouseover", function() {
-    this.classList.add("open");
-    var dropdownMenu = this.getElementsByClassName("dropdown-menu")[0];
-    dropdownMenu.style.display = "block";
-  });
 
-  dropdowns[i].addEventListener("mouseout", function() {
-    this.classList.remove("open");
-    var dropdownMenu = this.getElementsByClassName("dropdown-menu")[0];
-    dropdownMenu.style.display = "none";
-  });
-}
