9d89514db0b89229bc6a86cd6802fd1e6c8e353f
GNE13 <galimiel@gmail.com>
2023-04-30 15:30:20 +0300

    dropdown menu alter

--- static/index.js
+++ static/index.js
@@ -3,22 +3,7 @@ var today = new Date();
 var date = today.toLocaleDateString();
 // document.getElementById("localDate").innerHTML = date
 
-document.addEventListener("DOMContentLoaded", function() {
-  var dropdowns = document.getElementsByClassName("dropdown");
-
-  for (var i = 0; i < dropdowns.length; i++) {
-    dropdowns[i].addEventListener("mouseover", function() {
-      this.classList.add("open");
-      var dropdownMenu = this.getElementsByClassName("dropdown-menu")[0];
-      dropdownMenu.style.display = "block";
-    });
-    dropdowns[i].addEventListener("mouseout", function() {
-      this.classList.remove("open");
-      var dropdownMenu = this.getElementsByClassName("dropdown-menu")[0];
-      dropdownMenu.style.display = "none";
-    });
-  }
-});
+ 
 
 function addCourse() {
   alert("Corse was added succefuly !");
